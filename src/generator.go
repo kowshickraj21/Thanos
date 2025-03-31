dbba60991c3e86bb3455dbce75609f5300723456
kowshickraj21 <kowshickraj21.2005@gmail.com>
2025-03-31 07:07:42 +0530

    Printing in Assembly

--- src/generator.go
+++ src/generator.go
@@ -10,9 +10,24 @@ func Generate(node Node) {
 	buf.WriteString("section .text \n")
 	buf.WriteString("global _start \n")
 	buf.WriteString("_start: \n")
+
+	buf.WriteString("mov rax, 0X0A3831 ;\n")
+	buf.WriteString("push rax ;\n")
+	buf.WriteString("mov rdx, 3 ;\n")
+	buf.WriteString("call print \n")
+	buf.WriteString("pop rax ;\n")
+
 	buf.WriteString("mov rax, 60 ;\n")
 	buf.WriteString("mov rdi, 20 ;\n")
 	buf.WriteString("syscall ;\n")
+
+	buf.WriteString("print: \n")
+	buf.WriteString("mov rax, 1 ;\n")
+	buf.WriteString("mov rdi, 1 ;\n")
+	buf.WriteString("lea rsi, [rsp+8] ;\n")
+	buf.WriteString("syscall ;\n")
+	buf.WriteString("ret ;\n")
+
 	file, _ := os.Create("out.asm")
 	defer file.Close()
 
--- src/out.asm
+++ src/out.asm
@@ -1,6 +1,17 @@
 section .text 
 global _start 
 _start: 
+mov rax, 0X0A3831 ;
+push rax ;
+mov rdx, 3 ;
+call print 
+pop rax ;
 mov rax, 60 ;
 mov rdi, 20 ;
 syscall ;
+print: 
+mov rax, 1 ;
+mov rdi, 1 ;
+lea rsi, [rsp+8] ;
+syscall ;
+ret ;
